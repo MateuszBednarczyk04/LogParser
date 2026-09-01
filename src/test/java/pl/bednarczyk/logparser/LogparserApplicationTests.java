f943c58c8aa8753ef5fe50c17b79da575103442f
mateusz.bednarczyk <mateusz.bednarczyk04@wp.pl>
2026-09-01 23:51:14 +0200

    feat: init basic spring boot application and infrastructure

---
+++ src/test/java/pl/bednarczyk/logparser/LogparserApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bednarczyk.logparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogparserApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
